42358101735bfa91a3727ad0b3e654735a015c08
tahataha005 <taha.f.taha005@gmail.com>
2022-09-18 18:59:47 +0300

    Edit profile => picture

--- Front-end/javascript/indexScript.js
+++ Front-end/javascript/indexScript.js
@@ -5,12 +5,15 @@ window.onload = () => {
     const profileBtn = document.getElementById("profileBtn")
     const profilePage = document.getElementById("profile")
     const tweetscontainer = document.getElementById("tweets")
+    const editProfileBtn = document.getElementById("editProfile")
+    const editProfilePopup = document.querySelector(".editProfilePopup")
 
     homeBtn.addEventListener("click",() =>{
 
         homePage.style.display = "flex"
         tweetscontainer.style.display = "flex"
         profilePage.style.display = "none"
+        editProfilePopup.style.display = "none"
     })
 
     profileBtn.addEventListener("click",() =>{
@@ -18,7 +21,12 @@ window.onload = () => {
         homePage.style.display = "none"
         tweetscontainer.style.display = "none"
         profilePage.style.display = "flex"
+        editProfilePopup.style.display = "none"
     })
 
+    editProfileBtn.addEventListener("click", () =>{
+        editProfilePopup.style.display = "flex"
+
+    })
 
 }
